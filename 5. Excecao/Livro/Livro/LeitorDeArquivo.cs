e00a59ab06c3c546165cbb1d93f5f387e2a1c8e0
Camila Alves <camila12.silva@outlook.com>
2023-05-08 16:48:58 -0300

    update: exercícios

---
+++ 5. Excecao/Livro/Livro/LeitorDeArquivo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arquivo
+{
+    public class LeitorDeArquivo //: IDisposable
+    {
+        public string Arquivo { get; }
+
+        public LeitorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+
+            // throw new FileNotFoundException();
+            
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha. . .");
+            // exceção de entrada ou saída
+            throw new IOException();
+
+            return "Linha do arquivo";
+        }
+
+        public void Fechar()
+        {
+            Console.WriteLine("Fechando arquivo.");
+        }
+    }
+}
---
+++ 5. Excecao/Livro/Livro/Program.cs
@@ -0,0 +1,21 @@
+﻿using Arquivo;
+
+LeitorDeArquivo leitor = new LeitorDeArquivo("conts.txt");
+
+try
+{
+    
+    leitor.LerProximaLinha();
+    leitor.LerProximaLinha();
+    
+}
+catch(IOException)
+{
+    Console.WriteLine("Leitura interrompida!");
+    
+}
+finally
+{
+    // finally para continuar a execução dos comandos;
+    leitor.Fechar();
+}

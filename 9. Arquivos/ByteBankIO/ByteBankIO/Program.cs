a295c66396d8630f267e341b93d7f280f4bc7769
Camila Alves <camila12.silva@outlook.com>
2023-05-26 16:57:15 -0300

    :books: update

--- 9. Arquivos/ByteBankIO/ByteBankIO/Program.cs
+++ 9. Arquivos/ByteBankIO/ByteBankIO/Program.cs
@@ -1,12 +1,36 @@
 ﻿using ByteBankIO;
 using System.Text;
+using System.Xml;
 
 partial class Program
 {
     // quando eu uso o partial, ele vai entender que essa classe está separada em diversos arquivos.
     static void Main(string[] args)
     {
-        Console.WriteLine();
+        var enderecoDoArquivo = "contas.txt";
+
+        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDeArquivo);
+
+            //var linha = leitor.ReadLine();
+            //Console.WriteLine(linha);
+
+            // precisa tomar cuidado com esse método, pq ele carrega o arquivo de uma única vez.
+            //var texto = leitor.ReadToEnd();
+            //Console.WriteLine(texto);
+
+            // traz o 1º byte do nosso arquivo
+            //var numero = leitor.Read();
+            //Console.WriteLine(numero);
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+        }
+       
     }
 
 }

cc807aef369b3a2c5f7384ac793bc42b69c3d90b
Camila Alves <camila12.silva@outlook.com>
2023-06-05 14:32:50 -0300

    :books: update

--- 10. Nuget/consumidor_pacote/ConsumidorPacote/ConsumidorPacote/Program.cs
+++ 10. Nuget/consumidor_pacote/ConsumidorPacote/ConsumidorPacote/Program.cs
@@ -2,3 +2,10 @@
 
 Console.WriteLine(GeradorPix.GetChavePix());
 // funciocou
+
+var lista = GeradorPix.GetChavesPix(5);
+
+foreach (var chave in lista)
+{
+    Console.WriteLine(chave);
+}
---
+++ 10. Nuget/consumidor_pacote/TransformadorDeArquivos/TransformadorDeArquivos/ConverterArquivos.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System.Reflection.Metadata;
+using System.Xml;
+using System.Xml.Serialization;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace TransformadorDeArquivos
+{
+    public static class ConverterArquivos <T>
+    {
+
+        public static bool GetListEmXML(List<T> list)
+        {
+            if (list == null)
+            {
+                Console.WriteLine("Não há dados para conversão");
+            }
+            else
+            {
+                try
+                {
+                    var serializer = new XmlSerializer(typeof(List<T>));
+
+                    var enderecoDoNovoArquivo = "novoArquivoEmXml.xml";
+                    using (var fs = new FileStream(enderecoDoNovoArquivo, FileMode.Create))
+                    using (var escritor = new StreamWriter(fs))
+                    {
+
+                        serializer.Serialize(escritor, list);
+
+                    }
+                    Console.WriteLine("Arquivo salvo e convertido em XML");
+                    Console.ReadKey();
+
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+
+                
+                
+            }
+
+            return true;
+        }
+
+
+        public static bool GetListJson(List<T> list)
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Não há dados para conversão");
+            }
+            else
+            {
+                string json = JsonConvert.SerializeObject(list, Formatting.Indented);
+
+
+                try
+                {
+
+                    var endereco = "novoArquivoJson.json";
+                    using (var fs = new FileStream(endereco, FileMode.Create))
+                    using (var escritor = new StreamWriter(fs))
+                    {
+                        escritor.WriteLine(json);
+                    }
+                    Console.WriteLine("Arquivo salvo e convertido em json");
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+
+               
+            }
+
+            return true;
+
+        }
+    }
+}

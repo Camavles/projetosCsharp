09dd07850cf7fd4abcefb2383b94d8800f9a7177
Camila Alves <camila12.silva@outlook.com>
2023-06-05 17:01:39 -0300

    :books: update

---
+++ Exercicios/4_Revisao/RevisaoArquivos/RevisaoArquivos/Contas/ContaCorrente.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisaoArquivos.Contas
+{
+    public class ContaCorrente
+    {
+        private string numero_conta;
+
+        private int agencia;
+
+        private double saldo;
+
+        public Cliente titular;
+
+        public string Numero_conta
+        {
+            get { return numero_conta; }
+            set { numero_conta = value; }
+        }
+
+
+        public int Agencia
+        {
+            get { return agencia; }
+            set
+            {
+
+                if (agencia <= 0)
+                {
+                    Console.WriteLine("Número Inválido!");
+                }
+                else
+                {
+
+                    agencia = value;
+                }
+            }
+
+        }
+
+        public double Saldo
+        {
+            get { return saldo; }
+            set
+            {
+
+                if (value < 0)
+                {
+                    Console.WriteLine("Saldo não pode ser negativo");
+                }
+                else
+                {
+                    saldo = value;
+                }
+            }
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= 0 || valor > this.saldo)
+            {
+                Console.WriteLine("Valor inválido ou saldo insuficiente!");
+            }
+            else
+            {
+                this.saldo -= valor;
+                Console.WriteLine("Operação efetuada com sucesso!");
+            }
+
+            return true;
+        }
+
+
+        public double Depositar(double valor)
+        {
+            return this.saldo += valor;
+        }
+
+
+        public bool Transfere(ContaCorrente conta, double valor)
+        {
+            if (valor <= 0 || valor > this.saldo)
+            {
+                Console.WriteLine("Operação inválida!");
+            }
+            else
+            {
+                this.Sacar(valor);
+                conta.Depositar(valor);
+                Console.WriteLine("Operação realizada com sucesso!");
+            }
+
+            return true;
+        }
+
+        public ContaCorrente(int agencia, string numero_conta)
+        {
+            this.numero_conta = numero_conta;
+            this.agencia = agencia;
+        }
+
+        public ContaCorrente()
+        {
+            
+        }
+
+
+        public override string ToString()
+        {
+            return $"Conta: {numero_conta}, Agência: {agencia}, Saldo R$ {saldo}";
+        }
+
+    }
+}
---
+++ Exercicios/4_Revisao/RevisaoArquivos/RevisaoArquivos/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using RevisaoArquivos.Contas;
+using System.Xml.Serialization;
+
+TestaAplicacao();
+void TestaAplicacao()
+{
+    ContaCorrente conta1 = new ContaCorrente(23, "1010-X");
+    conta1.titular = new Cliente { Nome = "Camila", Cpf = "12345" };
+    //conta1.Depositar(50);
+
+    ContaCorrente conta2 = new ContaCorrente(24, "2020-Z");
+    conta2.titular = new Cliente { Nome = "Rapha", Cpf = "5892" };
+    //conta1.Transfere(conta2, 25);
+
+    List<ContaCorrente> listaDeContas = new List<ContaCorrente>() { conta1, conta2, new ContaCorrente(25, "4040-W") };
+
+    //foreach(var conta in listaDeContas)
+    //{
+    //    Console.WriteLine(conta);
+    //}
+
+    //serializando um arquivo em XML
+    //var serializer = new XmlSerializer(listaDeContas.GetType());
+
+    //var caminho = "arquivoTesteXML.xml";
+    //using (var fs = new FileStream(caminho, FileMode.Create))
+    //using (var escritor = new StreamWriter(fs))
+    //{
+    //    serializer.Serialize(escritor, listaDeContas);
+    //}
+    //Console.WriteLine("Arquivo Convertido");
+    //Console.ReadKey();
+
+
+    // Convertendo em Json
+    var json = JsonConvert.SerializeObject(listaDeContas, Formatting.Indented);
+    var endereco = "novoArqEmJson.json";
+
+    using(var fs = new FileStream(endereco, FileMode.Create))
+    using(var escritor = new StreamWriter(fs))
+    {
+        escritor.Write(json);
+    }
+    Console.WriteLine("Arquivo salvo em json");
+    Console.ReadKey();
+
+
+}

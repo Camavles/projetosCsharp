2b6253d3b46dbd8a9cf5e722a5ee8ec3abe952d8
Camila Alves <camila12.silva@outlook.com>
2023-06-16 13:55:07 -0300

    :books: update

---
+++ Exercicios/Modulo_13/Modulo_13/Pessoa.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo_13
+{
+    public class Pessoa
+    {
+		private string nome;
+
+		private string data_Nascimento;
+
+		private double altura;
+
+
+		public string Nome
+		{
+			get { return nome; }
+			set { nome = value; }
+		}
+
+		public string Data_Nascimento
+		{
+			get { return data_Nascimento; }
+			set { data_Nascimento = value; }
+		}
+
+		public double Altura
+		{
+			get { return altura; }
+			set { altura = value; }
+		}
+
+		// método para imprimir todos os dados;
+        public override string ToString()
+        {
+			return $"Nome: {nome}, Data de Nascimento: {data_Nascimento}, Altura: {altura}";
+        }
+
+		//método para calcular idade
+
+		public int CalcularIdade(int anoAtual)
+		{
+			var data = data_Nascimento;
+			var separaPorBarra = data.Split('/');
+			var anoNascimento = separaPorBarra[2];
+			var converteAnoNascimento = int.Parse(anoNascimento);
+
+			var idade = anoAtual - converteAnoNascimento;
+
+			return idade;
+		}
+	}
+
+
+
+
+}
---
+++ Exercicios/Modulo_13/Modulo_13/Program.cs
@@ -0,0 +1,11 @@
+﻿using Modulo_13;
+
+
+Pessoa pessoa = new Pessoa();
+pessoa.Nome = "Camila";
+pessoa.Data_Nascimento = "21/01/1997";
+pessoa.Altura = 1.62;
+var idade = pessoa.CalcularIdade(2023);
+
+Console.WriteLine($"{pessoa} / Essa é a idade: {idade}");
+

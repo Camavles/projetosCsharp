8ceae5deafd70ad909c01a5cbc675e5c5b9130db
Camila Alves <camila12.silva@outlook.com>
2023-07-14 14:24:38 -0300

    Config da Controller

--- 18. Web_API/UsuariosApi/UsuariosApi/Controllers/UsuarioController.cs
+++ 18. Web_API/UsuariosApi/UsuariosApi/Controllers/UsuarioController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+using UsuariosApi.Data;
 using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
 
 namespace UsuariosApi.Controllers
 {
@@ -7,12 +12,32 @@ namespace UsuariosApi.Controllers
     [Route("[Controller]")]
     public class UsuarioController : ControllerBase
     {
+        private IMapper _mapper;
+        private UserManager<Usuario> _userManager;
+
+        public UsuarioController(IMapper mapper, UserManager<Usuario> userManager = null)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+        }
 
         [HttpPost]
 
-        public IActionResult CadastraUsuario(CreateUsuarioDto dto)
+        // representa uma operação assíncrona que pode retornar uma valor 
+        // o tipo de retorno de um método assíncro prevcisa ser: nulo, Task Task<T>, IAsyncEnumerable<T>, IAsyncEnumerator<T>
+        public async Task<IActionResult> CadastraUsuario(CreateUsuarioDto dto)
         {
-            throw new NotImplementedException();
+            var usuario = _mapper.Map<Usuario>(dto);
+
+            
+            // esperamos o resultado dessa operação e isso é diferente de esperar o retorno de uma operação assíncrona
+            // o await aguarda pela execução do CreateAsync
+           IdentityResult resultado = await _userManager.CreateAsync(usuario, dto.Password);
+
+            if (resultado.Succeeded) return Ok("Usuário cadastrado");
+
+            throw new ApplicationException("Falha ao cadastrar usuário!");
+            
         }
     }
 }
--- 18. Web_API/UsuariosApi/UsuariosApi/Program.cs
+++ 18. Web_API/UsuariosApi/UsuariosApi/Program.cs
@@ -23,6 +23,8 @@ builder.Services
     .AddDefaultTokenProviders();
 
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
